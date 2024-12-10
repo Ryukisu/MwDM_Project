4c2d0e72384ae6f2d4fb50c1e26e44b0012140af
Ryukisu <129180189+Ryukisu@users.noreply.github.com>
2024-12-10 11:48:33 +0100

    Add files via upload

--- neural network.py
+++ neural network.py
@@ -7,8 +7,12 @@ from torch.utils.data import DataLoader
 
 # 1. Przygotowanie danych
 transform = transforms.Compose([
+    transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),  # Przycinanie i zmiana rozmiaru
+    transforms.RandomHorizontalFlip(),  # Losowe odbicie w poziomie
+    transforms.RandomRotation(15),  # Losowy obrót o ±15 stopni
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Zmiany koloru
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # Normalizacja
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),  # Normalizacja
     transforms.Lambda(lambda x: x.view(-1))  # Spłaszczenie obrazów
 ])
 
@@ -22,39 +26,76 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
 # 2. Definicja modelu
-class FullyConnectedNN(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(FullyConnectedNN, self).__init__()
-        self.fc_layers = nn.Sequential(
-            nn.Linear(input_size, 1024),
+# class FullyConnectedNN(nn.Module):
+#     def __init__(self, input_size, num_classes):
+#         super(FullyConnectedNN, self).__init__()
+#         self.fc_layers = nn.Sequential(
+#             nn.Linear(input_size, 1024),
+#             nn.ReLU(),
+#             # nn.Dropout(0.3),
+#             nn.Linear(1024, 512),
+#             nn.ReLU(),
+#             # nn.Dropout(0.3),
+#             nn.Linear(512, 256),
+#             nn.ReLU(),
+#             # nn.Dropout(0.3),
+#             nn.Linear(256, 128),
+#             nn.ReLU(),
+#             # nn.Dropout(0.3),
+#             nn.Linear(128, 64),
+#             nn.ReLU(),
+#             # nn.Dropout(0.3),
+#             nn.Linear(64, num_classes)
+#         )
+
+#     def forward(self, x):
+#         return self.fc_layers(x)
+
+
+# input_size = 32 * 32 * 3  # CIFAR-10 ma obrazy o wymiarach 32x32x3
+# num_classes = 10
+# model = FullyConnectedNN(input_size, num_classes)
+
+class CNNWithFullyConnected(nn.Module):
+    def __init__(self, num_classes):
+        super(CNNWithFullyConnected, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),  # 32x32x32
             nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(1024, 512),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 16x16x32
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 16x16x64
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 8x8x64
+            nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1),  # 8x8x128
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)  # 4x4x128
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(4 * 4 * 256, 512),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(512, 256),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(64, num_classes)
+            nn.Linear(256, num_classes)
         )
 
     def forward(self, x):
-        return self.fc_layers(x)
+        x = x.view(-1, 3, 32, 32)  # Zmieniamy kształt danych na 3x32x32
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)  # Spłaszczamy dane
+        x = self.fc_layers(x)
+        return x
 
 
-input_size = 32 * 32 * 3  # CIFAR-10 ma obrazy o wymiarach 32x32x3
 num_classes = 10
-model = FullyConnectedNN(input_size, num_classes)
+model = CNNWithFullyConnected(num_classes)
+
 
 # 3. Definicja funkcji straty i optymalizatora
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+# optimizer = optim.SGD(model.parameters(), lr=0.001)
 
 
 # 4. Trening modelu
@@ -97,5 +138,5 @@ def evaluate_model(model, test_loader):
 
 
 # Uruchomienie treningu i testu
-train_model(model, train_loader, criterion, optimizer, num_epochs=10)
+train_model(model, train_loader, criterion, optimizer, num_epochs=15)
 evaluate_model(model, test_loader)
